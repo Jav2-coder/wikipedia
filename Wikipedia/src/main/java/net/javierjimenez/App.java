3e86e07e2c2203c89c215504e208a61588c0f2e0
alumne1daw <alumne1daw@localhost>
2016-02-16 17:47:02 +0100

    Commit: Programa finalizado - falta documentar

--- Wikipedia/src/main/java/net/javierjimenez/App.java
+++ Wikipedia/src/main/java/net/javierjimenez/App.java
@@ -1,13 +1,41 @@
 package net.javierjimenez;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 /**
- * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("colors.txt"), "utf-8"));
+		
+		Document doc = Jsoup.connect("https://ca.wikipedia.org/wiki/Llista_de_colors").get();
+
+		Elements tbody = doc.select("tbody");
+
+		Elements tr = tbody.select("tr");
+
+		for (int i = 1; i < tr.size(); i++) {
+
+			Elements th = tr.get(i).select("th");
+
+			Elements td = tr.get(i).select("td");
+
+			writer.write("INSERT INTO colors (nom, colorhex, red, green, blue) VALUES (“" + th.text() + "”, “"
+					+ td.get(1).text() + "”, " + td.get(2).text() + ", " + td.get(3).text() + ", " + td.get(4).text()
+					+ ");\n");
+		}
+		
+		writer.close();
+		
+	}
 }
